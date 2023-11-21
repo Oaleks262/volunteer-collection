bb4edb209ef1d6b0b2ee3e6eda972970ce6e0f0b
Oleksand-Zvirych <oaleks262@gmail.com>
2023-11-21 11:14:17 +0200

    edit

--- src/assets/api/api.js
+++ src/assets/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://volunteer-collection-api.onrender.com/";
+axios.defaults.baseURL = "https://volunteer-collection-api.onrender.com/api/";
 
 
 export const lendingData = {
